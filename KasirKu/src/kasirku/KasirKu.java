0baac384faec557fb55dc2439973e828603788fc
Rayhan Muhammad Adha <67584512+Mr22XX@users.noreply.github.com>
2023-11-11 04:37:06 +0700

    Add files via upload

---
+++ KasirKu/src/kasirku/KasirKu.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package kasirku;
+
+/**
+ *
+ * @author Mr22xx
+ */
+public class KasirKu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new kasirFrame().show();
+    }
+    
+}
